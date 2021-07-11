7f4e10770027312aaa416cf4070ef83629652f1b
wns312 <wns312@gmail.com>
2021-07-11 18:40:30 +0900

    웹소켓 테스트

---
+++ src/main/java/jyk/websocket/socket/GreetingController.java
@@ -0,0 +1,57 @@
+package jyk.websocket.socket;
+
+import lombok.Data;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+    static class Greeting {
+
+        private String content;
+
+        public Greeting() {
+        }
+
+        public Greeting(String content) {
+            this.content = content;
+        }
+
+        public String getContent() {
+            return content;
+        }
+    }
+    @Data
+    static class HelloMessage {
+
+        private String name;
+
+        public HelloMessage() {
+        }
+
+        public HelloMessage(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
+}
